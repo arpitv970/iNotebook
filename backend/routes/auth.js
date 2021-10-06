073666299e7fed50c49db6812547e496e2048316
Arpit Verma <arpitv970@gmail.com>
2021-10-06 22:22:08 +0530

    Added new features
    - Now it can avoid duplication of email address
    - Comments added for development efficiency

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,24 +3,39 @@ const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
-// Creating User using: POST "/api/auth". Doesn't required Auth
-router.post('/', [
+// Creating User using: POST "/api/auth/createuser". No Login required
+router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password length must be atleast 5 characters').isLength({ min: 5 }),
-], (req, res)=>{
+], async (req, res)=>{
+    // If there are errors, return bad request & errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    
+    // Check whether the user with same email exists already
+    try {
 
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-      }).then(user => res.json(user))
-      .catch(err=>{console.log(err)
-    res.json({error: 'Please enter a unique value for email', message: err.message})})
+      let user = await User.findOne({email: req.body.email}); 
+      if (user){
+        return req.status(400).json({error: "Sorry a user with this email already exists"})
+      }
+
+      // Creates a new user
+      user = await User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+        })
+        
+      res.json(user)
+          
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some error occured");
+    }
 })
 
 module.exports = router
