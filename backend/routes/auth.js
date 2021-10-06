ec9cab2dfc7d7f9bc4a7351c466b5bd49817fd3c
Arpit Verma <arpitv970@gmail.com>
2021-10-06 21:25:29 +0530

    Added authentication
    - validation mechanism is added, so user can enter data correctly in
      repective data field
    - this will make security a bit stronger

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 // Creating User using: POST "/api/auth". Doesn't required Auth
-router.post('/', (req, res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save()
-    res.send(req.body);
+router.post('/', [
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password length must be atleast 5 characters').isLength({ min: 5 }),
+], (req, res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      }).then(user => res.json(user))
+      .catch(err=>{console.log(err)
+    res.json({error: 'Please enter a unique value for email', message: err.message})})
 })
 
 module.exports = router
