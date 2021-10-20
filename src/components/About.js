74880df15f3e2581901f08fe8cdb9040f042ff62
Arpit Verma <arpitv970@gmail.com>
2021-10-20 23:34:32 +0530

    Removed unwanted code lines

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,9 @@
 import React, { useContext, useEffect } from 'react'
-import noteContext from '../context/notes/noteContext'
 
 const About = () => {
-    const a = useContext(noteContext)
-    useEffect(() => {
-        a.update();
-    }, [])
     return (
         <div>
-            This is About Page: {a.state.name} and he is in class {a.state.class}
+            This is About Page
         </div>
     )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,21 +2,8 @@ import noteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props)=>{
-    const s1 = {
-        "name": "Arpit",
-        "class": "5b"
-    }
-    const [state, setstate] = useState(s1);
-    const update = ()=>{
-        setTimeout(() => {
-            setstate({
-                "name": "Dev",
-                "class": "12b"
-            })
-        }, 1000);
-    }
     return(
-        <noteContext.Provider value={{state, update}}>
+        <noteContext.Provider value={{}}>
             {props.children}
         </noteContext.Provider>
     )
