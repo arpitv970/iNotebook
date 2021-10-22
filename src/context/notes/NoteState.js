7cbb7ac67066208dd32f0e946912dc3f55e80cc4
Arpit Verma <arpitv970@gmail.com>
2021-10-22 14:44:01 +0530

    Basic changes done

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -34,6 +34,8 @@ const NoteState = (props) => {
       },
       body: JSON.stringify({ title, description, tag }),
     });
+    const json = await response.json();
+    console.log(json)
 
     console.log("Adding a New Note");
     const note = {
@@ -60,7 +62,7 @@ const NoteState = (props) => {
             "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjE2NTU3YzFkMWUxMGNkMzg2YjMyNjEzIn0sImlhdCI6MTYzNDc1NTA1OX0.QL3HvloQpSMXw4zbvv5CdclP0Z5AlwvOPQZAl5nndaM",
         }
       });
-      const json = response.json();
+      const json = await response.json();
       console.log(json)
     console.log("Deleting the note with id: " + id);
     const newNotes = notes.filter((note) => {
@@ -73,7 +75,7 @@ const NoteState = (props) => {
   const editNote = async (id, title, description, tag) => {
     // API Call
     const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
-      method: "POST",
+      method: "PUT",
       headers: {
         "Content-Type": "application/json",
         "auth-token":
@@ -81,17 +83,22 @@ const NoteState = (props) => {
       },
       body: JSON.stringify({ title, description, tag }),
     });
-    const json = response.json();
+    const json = await response.json();
+    console.log(json);
 
+    let newNotes = JSON.parse(JSON.stringify(notes))
     // Logic to edit in client
-    for (let index = 0; index < notes.length; index++) {
-      const element = notes[index];
+    for (let index = 0; index < newNotes.length; index++) {
+      const element = newNotes[index];
       if (element._id === id) {
-        element.title = title;
-        element.description = description;
-        element.tag = tag;
+        newNotes[index].title = title;
+        newNotes[index].description = description;
+        newNotes[index].tag = tag;
+        break;
       }
     }
+    console.log(id, notes)
+    setNotes(newNotes);
   };
   return (
     <noteContext.Provider value={{ notes, addNote, deleteNote, editNote, getNotes }}>
